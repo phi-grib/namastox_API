45dea37b6b1e542b86662ef1e87607d8d535ba48
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-02-07 15:28:46 +0100

    change flask to flask_api

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask,jsonify
+# from flask_api import FlaskAPI
+# from flask_api import status as Status
 app = Flask(__name__)
 url_base = "/namastox/"
 version = "v1/"
