23d9bb59c720fd5eee85d05c424b83237b166738
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-02-08 17:11:20 +0100

    change flask to flask api

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
-from flask import Flask,jsonify
+from flask import jsonify
 from flask_cors import CORS, cross_origin
-# from flask_api import FlaskAPI
+from flask_api import FlaskAPI
 # from flask_api import status as Status
-app = Flask(__name__)
+app = FlaskAPI(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 url_base = "/namastox/"
