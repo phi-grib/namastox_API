d519ac7bf560dd3032a8f957c8bf82dcfa8648a7
manuelpastor <manuel.pastor@upf.edu>
2023-02-12 22:23:20 +0100

    rename API

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ from settings import *
 from namastox import update
 
 # GET RESULTS LIST of RA
-@app.route(f'{url_base}{version}update_info/<string:ra_name>',methods=['POST'])
+@app.route(f'{url_base}{version}general_info/<string:ra_name>',methods=['PUT'])
 @cross_origin()
 def updateGeneralInfo(ra_name):
     #TODO extract input_dict from payload
@@ -13,9 +13,10 @@ def updateGeneralInfo(ra_name):
     else:
         return f'Failed to update General Info for {ra_name} with error: {data}', 500
 
-@app.route(f'{url_base}{version}update_result/<string:ra_name>/<int:step>',methods=['POST'])
+@app.route(f'{url_base}{version}result/<string:ra_name>',methods=['PUT'])
+@app.route(f'{url_base}{version}result/<string:ra_name>/<int:step>',methods=['PUT'])
 @cross_origin()
-def updateResult(ra_name, step):
+def updateResult(ra_name, step=None):
     #TODO extract input_dict from payload
     input_dict = {}
     success, data = update.action_update_result(ra_name, step, input_dict)
