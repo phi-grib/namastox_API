61818c30f6f654d58ed36ce9c7457148a15d6268
manuelpastor <manuel.pastor@upf.edu>
2023-02-13 22:15:02 +0100

    improvement in update endpoints

--- settings.py
+++ settings.py
@@ -1,8 +1,7 @@
 from flask import jsonify
-from flask import request
 from flask_cors import CORS, cross_origin
 from flask_api import FlaskAPI
-# from flask_api import status as Status
+
 app = FlaskAPI(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
--- update.py
+++ update.py
@@ -1,20 +1,19 @@
 from settings import *
 from namastox import update
 from flask import request
-import json
 
-# GET RESULTS LIST of RA
+# PUT GENERAL_INFO
 @app.route(f'{url_base}{version}general_info/<string:ra_name>',methods=['PUT'])
 @cross_origin()
 def updateGeneralInfo(ra_name):
-    #TODO extract input_dict from payload
-    input_dict = {}
-    success, data = update.action_update_general_info(ra_name, input_dict)
+    input_dict = request.form['general']
+    success, data = update.action_update_general_info(ra_name, {'general':input_dict})
     if success:
         return data
     else:
         return f'Failed to update General Info for {ra_name} with error: {data}', 500
 
+# PUT RESULT
 @app.route(f'{url_base}{version}result/<string:ra_name>',methods=['PUT'])
 @app.route(f'{url_base}{version}result/<string:ra_name>/<int:step>',methods=['PUT'])
 @cross_origin()
