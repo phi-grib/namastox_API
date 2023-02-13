0a49d3e6e061c4f786151bdcf744dc90563d4f32
manuelpastor <manuel.pastor@upf.edu>
2023-02-13 09:36:19 +0100

    first PUT endpoints

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from flask import request
 from flask_cors import CORS, cross_origin
 from flask_api import FlaskAPI
 # from flask_api import status as Status
--- update.py
+++ update.py
@@ -1,5 +1,7 @@
 from settings import *
 from namastox import update
+from flask import request
+import json
 
 # GET RESULTS LIST of RA
 @app.route(f'{url_base}{version}general_info/<string:ra_name>',methods=['PUT'])
@@ -17,9 +19,8 @@ def updateGeneralInfo(ra_name):
 @app.route(f'{url_base}{version}result/<string:ra_name>/<int:step>',methods=['PUT'])
 @cross_origin()
 def updateResult(ra_name, step=None):
-    #TODO extract input_dict from payload
-    input_dict = {}
-    success, data = update.action_update_result(ra_name, step, input_dict)
+    input_dict = request.form['result']
+    success, data = update.action_update_result(ra_name, step, [{'result':input_dict}])
     if success:
         return data
     else:
