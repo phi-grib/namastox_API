df3ca7095bb320cd399e7dbf682d7928291c0857
Manuel Pastor <manuel.pastor@upf.edu>
2023-04-26 19:35:09 +0200

    minor changes

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from status import *
 from notes import *
 from update import *
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return app.send_static_file(path)
+    else:
+        return app.send_static_file('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
+
 
