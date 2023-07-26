05d3def7ab1c4569cc34235fe80bfb5eb1a1de9e
Manuel Pastor <manuel.pastor@upf.edu>
2023-07-26 13:38:29 +0200

    added cross origin to some routes

--- results.py
+++ results.py
@@ -14,6 +14,7 @@ def getResults(ra_name, step=None):
         return json.dumps(f'Failed to obtain results for {ra_name} step {step} with error: {data}'), 500, {'ContentType':'application/json'} 
 
 @app.route(f'{url_base}{version}result/<string:ra_name>/<string:result_id>',methods=['GET'])
+@cross_origin()
 def getResult(ra_name, result_id):
     success, data = results.action_result(ra_name, result_id, out='json')
     if success:
@@ -22,6 +23,7 @@ def getResult(ra_name, result_id):
         return json.dumps(f'Failed to obtain result {result_id} for {ra_name} with error: {data}'), 500, {'ContentType':'application/json'} 
     
 @app.route(f'{url_base}{version}task/<string:ra_name>/<string:result_id>',methods=['GET'])
+@cross_origin()
 def getTask(ra_name, result_id):
 
     success, data = results.action_task(ra_name, result_id)
@@ -31,6 +33,7 @@ def getTask(ra_name, result_id):
         return json.dumps(f'Failed to obtain task {result_id} for {ra_name} with error: {data}'), 500, {'ContentType':'application/json'} 
 
 @app.route(f'{url_base}{version}pending_tasks/<ra_name>',methods=['GET'])
+@cross_origin()
 def getPendingTasks(ra_name):
     success, data = results.action_pendingTasks(ra_name)
     if success:
@@ -39,6 +42,7 @@ def getPendingTasks(ra_name):
         return json.dumps(f'Failed to obtain pending tasks for {ra_name} with error: {data}'), 500, {'ContentType':'application/json'} 
 
 @app.route(f'{url_base}{version}pending_task/<ra_name>/<string:result_id>',methods=['GET'])
+@cross_origin()
 def getPendingTask(ra_name, result_id):
     success, data = results.action_pendingTask(ra_name, result_id)
     if success:
