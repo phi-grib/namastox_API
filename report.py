4fa948b5202c1b2999b7f576d37bab70e9b5ac80
Manuel Pastor <manuel.pastor@upf.edu>
2024-02-14 18:29:37 +0100

    progress reporting

--- report.py
+++ report.py
@@ -6,8 +6,8 @@ import json
 @app.route(f'{url_base}{version}report/<string:ra_name>/<string:report_format>',methods=['GET'])
 @cross_origin()
 def reportRA(ra_name, report_format):
-    success, report_file = report.action_report (ra_name, report_format)
+    success, result = report.action_report (ra_name, report_format)
     if success:
-        return send_file(report_file, as_attachment=True)
+        return send_file(result, as_attachment=True)
     else:
-        return json.dumps(f'Failed to report {ra_name}'), 500, {'ContentType':'application/json'} 
+        return json.dumps(f'Failed to report {ra_name}, with error: {result}'), 500, {'ContentType':'application/json'} 
