d97f346ca9dc9e7339a0abeaa9205e736bb8b45f
Manuel Pastor <manuel.pastor@upf.edu>
2024-02-16 09:37:15 +0100

    notes handling

--- notes.py
+++ notes.py
@@ -8,30 +8,28 @@ import json
 def getNotes(ra_name, step=None):
     success, data = notes.action_notes(ra_name, step, out='json')
     if success:
-        return data
+        return json.dumps(data)
     else:
         return f'Failed to obtain notes for {ra_name} {step}', 500
 
 @app.route(f'{url_base}{version}note/<string:ra_name>/<string:note_id>',methods=['GET'])
 def getNote(ra_name, note_id):
-    success, data = notes.action_note(ra_name, note_id, out='json')
+    success, data = notes.action_note(ra_name, note_id)
     if success:
-        return data
+        return json.dumps(data)
     else:
         return f'Failed to obtain note {note_id} for {ra_name}', 500
     
 @app.route(f'{url_base}{version}note/<string:ra_name>',methods=['PUT'])
 def putNote(ra_name):
-
     note = {}
-
-    if 'note' in request.form:
+    if 'id' in request.form:
         note['id'] = request.form['id']
         note['title'] = request.form['title']
         note['date'] = request.form['date']
         note['text'] = request.form['text']
     else:
-        note = {'id':'MY', 'title':'MYTITLE', 'date':'MYDATE', 'text': 'MYTEXT'}
+        return 'No note found', 500
 
     success, data = notes.action_note_add(ra_name, note)
     if success:
