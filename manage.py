6eaf759262d0553251ede854c415df4b3013bdc3
Manuel Pastor <manuel.pastor@upf.edu>
2024-02-23 20:10:03 +0100

    custom workflow implementation

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from settings import *
-from namastox import manage, report
+from namastox import manage
 
 import json
 import os
--- update.py
+++ update.py
@@ -1,7 +1,13 @@
 from settings import *
 from namastox import update
+from namastox import manage
 from flask import request
-import json
+import json, os
+from werkzeug.utils import secure_filename
+
+def allowed_attachment(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in {'csv'}
 
 # PUT GENERAL_INFO
 @app.route(f'{url_base}{version}general_info/<string:ra_name>',methods=['PUT'])
@@ -9,6 +15,24 @@ import json
 def updateGeneralInfo(ra_name):
     input_string = request.form['general']
     input_dict = json.loads(input_string)
+
+    # check if the post request has the file part
+    if 'custom_workflow_file' in request.files:
+        file = request.files['custom_workflow_file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return json.dumps(f'Failed to upload file, empty file nama'), 500, {'ContentType':'application/json'} 
+        
+        if file and allowed_attachment(file.filename):
+            filename = secure_filename(file.filename)
+            filename = filename.replace (' ','_')
+            success, data = manage.getPath (ra_name)
+            if not success:
+                return json.dumps(f'Failed to upload file, unable to access repository'), 500, {'ContentType':'application/json'} 
+
+            file.save(os.path.join(data, filename))
+
     success, data = update.action_update_general_info(ra_name, {'general':input_dict})
     if success:
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
