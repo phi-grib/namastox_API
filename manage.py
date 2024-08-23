ae8d41b0610ebcab1040fbe987d6cef500168547
manuelpastor <manuel.pastor@upf.edu>
2024-08-23 11:50:15 +0200

    new model import endpoint

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import os
 import tempfile
 import shutil
 from werkzeug.utils import secure_filename
+from flame.manage import action_import
 
 # GET LIST of RA
 @app.route(f'{url_base}{version}list',methods=['GET'])
@@ -385,3 +386,39 @@ def putTable(ra_name):
         
     else:
         return json.dumps({'success:': False, 'error': 'Failed to upload file, incorrect file or file type'}), 500, {'ContentType':'application/json'} 
+    
+# IMPORT MODEL
+@app.route(f'{url_base}{version}import_model',methods=['POST'])
+@cross_origin()
+def importMadel():
+
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return json.dumps(f'Failed to upload file, no file information found'), 500, {'ContentType':'application/json'} 
+    
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        return json.dumps(f'Failed to upload file, empty filename'), 500, {'ContentType':'application/json'} 
+    
+    if file and allowed_import(file.filename):
+        filename = secure_filename(file.filename)
+        filename = filename.replace (' ','_')
+
+        data = manage.getModelPath ()
+        pathname = os.path.join(data, filename)
+        file.save(pathname)
+
+        # success, values, uncertainties = manage.getTableContents(pathname)
+        success, result = action_import(pathname)
+        print (success, result)
+        if success:
+            return json.dumps({'success':True, 'message': result}), 200, {'ContentType':'application/json'} 
+        else:
+            return json.dumps({'success':False, 'message': result}), 500, {'ContentType':'application/json'} 
+        
+    else:
+        return json.dumps({'success:': False, 'error': 'no suitable file'}), 500, {'ContentType':'application/json'} 
+    
+    
